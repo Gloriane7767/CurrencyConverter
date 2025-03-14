12bf0cbba411b565501e89ddba0897cfb0b7bbaa
Glory Foncha <gloriane7767@gmail.com>
2025-03-14 22:53:50 +0100

    Created the Currencies Intake and Take input from Users.

--- src/main/java/currencyConverter.java
+++ src/main/java/currencyConverter.java
@@ -6,3 +6,16 @@ import java.util.Scanner;
 
 public class currencyConverter {
     public static void main (String[] args) {
+
+        //Currencies Intake
+        System.out.println("1 SEK");
+        System.out.println("2 USD");
+        System.out.println("3 EURO");
+
+        //Take input from Users
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Choose the Currency");
+        int choice = scanner.nextInt();
+
+        System.out.println("Enter the Amount");
+        double amount = scanner.nextDouble();
