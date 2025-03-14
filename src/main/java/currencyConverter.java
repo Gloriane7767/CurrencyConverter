21801082be5d2773af735a4dee911c53b3152ca1
Glory Foncha <gloriane7767@gmail.com>
2025-03-14 22:57:50 +0100

    Completed the Convert the Amount with switch.

--- src/main/java/currencyConverter.java
+++ src/main/java/currencyConverter.java
@@ -19,3 +19,20 @@ public class currencyConverter {
 
         System.out.println("Enter the Amount");
         double amount = scanner.nextDouble();
+
+        //Convert the Amount
+        switch (choice) {
+            case 1:
+                SEK_to_other(amount);
+                break;
+            case 2:
+                USD_to_other(amount);
+                break;
+            case 3:
+                EURO_to_other(amount);
+                break;
+            default:
+                System.out.println("Invalid Choice");
+        }
+    }
+
